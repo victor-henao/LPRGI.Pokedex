af38857cb9b678d4c015e204c177b3d70f465ca9
Samy <Samy@DESKTOP-7D157VT>
2020-12-30 18:21:10 +0100

    Modification message d'entrée A réfléchir pour le string equal avec 'exit' + Réfléchir à la saisie première qui renvoie une erreur si ce n'est pas le nom d'un pokémon.

--- LPRGI.Pokedex/Program.cs
+++ LPRGI.Pokedex/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LPRGI.Pokedex
 {
@@ -9,8 +10,18 @@ namespace LPRGI.Pokedex
             using var pokedexCient = new Request.PokedexClient();
             try
             {
-                Console.WriteLine(pokedexCient.GetPokemonAsync("bulbasaur").Result);
-                Console.WriteLine(pokedexCient.GetPokemonAsync("ditto").Result);
+                string input = string.Empty;
+                Console.WriteLine("Entrez le nom ou le numéro d'un pokémon, 'exit' pour sortir");
+                input = Console.ReadLine();
+                bool isDigitPresent = input.Any(c => char.IsDigit(c));
+                while (!isDigitPresent)
+                {
+                    //!string.Equals(input,"exit", StringComparison.CurrentCultureIgnoreCase) ||
+
+                    Console.WriteLine(pokedexCient.GetPokemonAsync(input).Result);
+                    input = Console.ReadLine();
+                    isDigitPresent = input.Any(c => char.IsDigit(c));
+                }
             }
             catch (AggregateException e)
             {
